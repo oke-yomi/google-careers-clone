e0ed85e9c637dac43ad9c0ccde567cd587e13eb8
oke-yomi <okeoluwayomi9@gmail.com>
2022-08-23 03:49:09 +0100

    headline component and test completed

--- tests/unit/components/Headline.test.js
+++ tests/unit/components/Headline.test.js
@@ -4,44 +4,44 @@ import { mount } from "@vue/test-utils";
 import Headline from "@/components/Headline";
 
 describe("Headline", () => {
-  it("displays introductory action verb", () => {
+  beforeEach(() => {
     jest.useFakeTimers("legacy");
+  });
 
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+  // NOTE: beforeEach and afterEach is written to fix DRY method for each test, but in cases of long tests, it would be easier to repeat some of your code for convenience
+
+  it("displays introductory action verb", () => {
     const wrapper = mount(Headline);
     const actionPhrase = wrapper.find("[data-test='action-phrase']");
 
     expect(actionPhrase.text()).toBe("Build for everyone");
-    jest.useRealTimers();
   });
 
   it("changes action verb at a consistent interval", () => {
-    jest.useFakeTimers("legacy");
-    mount(Headline);
+    // jest.useFakeTimers("legacy"); --- runs before Headline is mounted
 
+    mount(Headline);
     expect(setInterval).toHaveBeenCalled();
-    jest.useRealTimers();
+
+    // jest.useRealTimers(); --- after each test
   });
 
   it("swaps action verb after first Interval", async () => {
-    jest.useFakeTimers("legacy");
-
     const wrapper = mount(Headline);
     jest.runOnlyPendingTimers();
-
     await nextTick();
-    const actionPhrase = wrapper.find("[data-test='action-phrase']");
 
+    const actionPhrase = wrapper.find("[data-test='action-phrase']");
     expect(actionPhrase.text()).toBe("Create for everyone");
-    jest.useRealTimers();
   });
 
   it("removes interval when component disappears", () => {
-    jest.useFakeTimers("legacy");
-
     const wrapper = mount(Headline);
     wrapper.unmount();
 
     expect(clearInterval).toHaveBeenCalled();
-    jest.useRealTimers();
   });
 });
