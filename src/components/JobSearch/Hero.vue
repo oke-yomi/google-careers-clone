363017ffcc8138d72c138fbded8006aad3e0c479
oke-yomi <okeoluwayomi9@gmail.com>
2022-08-25 03:17:52 +0100

    refactoring codebase

---
+++ src/components/JobSearch/Hero.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+    <section class="flex flex-col h-screen pt-10 pb-20">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1"></div>
+
+        <div class="col-start-2 col-span-5">
+          <headline />
+          <job-search-form />
+        </div>
+
+        <div class="col-start-7 col-span-5 self-center justify-self-center">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/2367px-Vue.js_Logo_2.svg.png"
+            class="w-80 h-80 object-contain"
+          />
+        </div>
+
+        <div class="col-start-12 col-span-1"></div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import Headline from "@/components/JobSearch/Headline.vue";
+import JobSearchForm from "@/components/JobSearch/JobSearchForm.vue";
+
+export default {
+  name: "Hero",
+  components: {
+    Headline,
+    JobSearchForm,
+  },
+};
+</script>
+
+<style scoped></style>
+
+<!-- Vue lifecycle
+  beforeCreate() {
+    console.log("Component is about to be created");
+  },
+  created() {
+    console.log("Component has been created");
+  },
+  beforeMount() {
+    console.log("Component is about to be mounted");
+  },
+  mounted() {
+    console.log("Component has been mounted");
+  },
+ -->
--- tests/unit/components/Navigation/SubNav.test.js
+++ tests/unit/components/Navigation/SubNav.test.js
@@ -1,6 +1,6 @@
 import { mount } from "@vue/test-utils";
 
-import SubNav from "@/components/SubNav.vue";
+import SubNav from "@/components/Navigation/SubNav.vue";
 
 describe("SubNav", () => {
   describe("when user is on jobs page", () => {
